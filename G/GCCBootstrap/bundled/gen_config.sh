d1f94cc5bb9287a9706b28abe7c67b8114076fba
Elliot Saba <staticfloat@gmail.com>
2022-04-13 19:55:42 -0400

    Try building a `GCCBootstrap_jll` using `crosstool-ng`
    TODO before merging:
    
    * Cleanup `glibc` patches!  Note that we're building against glibc 2.19
      on all platforms right now; this is fine, because this glibc is not
      what will be used in production, and it mostly affects what version of
      glibc must be available on the _host_ system when compiling, not on the
      _target_ system when running things built with this compiler.  So by
      using 2.19 here, we simply make it impossible to use BinaryBuilder on
      ancient CentOS systems, which I'm okay with.
    
    * Build for windows; supposedly we can build against mingw32.

---
+++ G/GCCBootstrap/bundled/gen_config.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Start wtih things that are always true
+cat <<-EOF
+CT_CONFIG_VERSION=4
+
+# Always build GCC v9.X
+CT_GCC_V_9=y
+
+CT_BUILD="${MACHTYPE}"
+CT_PREFIX_DIR="${prefix}"
+CT_PREFIX_DIR_RO=n
+CT_RM_RF_PREFIX_DIR=n
+
+# Don't download any tarballs; we should have taken care of that already
+CT_FORBID_DOWNLOAD=y
+CT_LOCAL_TARBALLS_DIR="${WORKSPACE}/srcdir"
+
+# We always want to build g++
+CT_CC_LANG_CXX=y
+EOF
+
+# Handle OS stuff
+case "${bb_target}" in
+    *linux*)
+        cat <<-EOF
+# We're building against linux, and always use an older kernel
+# version so that we are maximally compatible.
+CT_KERNEL_LINUX=y
+CT_LINUX_V_4_1=y
+
+# We don't like the 'unknown' for Linux
+CT_OMIT_TARGET_VENDOR=y
+CT_TARGET_VENDOR=
+EOF
+        ;;
+    *)
+        echo "Unhandled OS '${bb_target}'" >&2
+        exit 1
+        ;;
+esac
+
+# Handle arch stuff
+case "${bb_target}" in
+    arm*)
+        echo "CT_ARCH_ARM=y"
+        ;;
+    aarch64*)
+        echo "CT_ARCH_ARM=y"
+        echo "CT_ARCH_64=y"
+        ;;
+    i686*)
+        echo "CT_ARCH_X86=y"
+        echo "CT_ARCH_ARCH=\"pentium4\""
+        ;;
+    x86_64*)
+        echo "CT_ARCH_X86=y"
+        echo "CT_ARCH_64=y"
+        ;;
+    *)
+        echo "ERROR: Unhandled arch '${bb_target}'" >&2
+        exit 1
+esac
+
+# Handle libc stuff
+case "${bb_target}" in
+    *gnu*)
+#        case "${bb_target}" in
+#            arm*|aarch64*)
+#                echo "CT_GLIBC_V_2_17=y"
+#                ;;
+#            x86_64*|i686*)
+#                echo "CT_OBSOLETE=y"
+#                echo "CT_GLIBC_V_2_12_1=y"
+#                echo "CT_GLIBC_VERSION=\"2.12.1\""
+#                ;;
+#            powerpc64le*)
+        echo "CT_GLIBC_V_2_19=y"
+#                ;;
+#            *)
+#                echo "ERROR: Unhandled glibc arch '${bb_target}'" >&2
+#                exit 1
+#        esac
+        ;;
+    *)
+        echo "ERROR: Unhandled libc '${bb_target}'" >&2
+        exit 1
+        ;;
+esac
